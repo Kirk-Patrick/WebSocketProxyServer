f71c6c2cde3b553d47e37c7b57ce5ba29dc2ca99
vtechjm <vtechjm@gmail.com>
2020-09-04 23:26:34 -0500

    Initial commit add websocket dependencieds SockJS

---
+++ src/main/java/com/vtechjm/websocketserver/WebSocketProxyServerAppApplication.java
@@ -0,0 +1,13 @@
+package com.vtechjm.websocketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketProxyServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketProxyServerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vtechjm/websocketserver/WebSocketProxyServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtechjm.websocketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketProxyServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
